2f357f365c4ee426725c00a9dddf2956999abdd3
Jules Sabater <jules.sab132@outlook.fr>
2023-01-16 17:29:27 +0100

    Fix server crashing when tracking device falls asleep

--- Assets/ViViCam_server/udp_server.py
+++ Assets/ViViCam_server/udp_server.py
@@ -1,5 +1,3 @@
-# Emulate the triad_openvr server
-
 import triad_openvr
 import time
 import sys
@@ -9,10 +7,26 @@ import ctypes
 winmm = ctypes.WinDLL('winmm')
 winmm.timeBeginPeriod(1)
 
+remove_elements_flag = False
+start = time.time()
+
+# Removes inactive tracking devices
+def cleanup_tracked_list():
+    tracked_error_list = []
+
+    for device_string in v.devices.keys():
+        if (v.devices[device_string].get_pose_quaternion() is None):
+            print("[Warning]: Tracking device \"" + device_string + "\" is inactive! Removing from list.")
+            tracked_error_list.append(device_string)
+
+    for device in tracked_error_list:
+        v.devices.pop(device)
+
+    remove_elements_flag = False
+
+##### INIT #####
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = ('127.0.0.1', 8051)
-
-# floats = [0.1253, 0.157446, 0.968451, 0.87453618, 0.8968, 0.84865465, 0.843872]
 
 v = triad_openvr.triad_openvr()
 v.print_discovered_objects()
@@ -27,20 +41,13 @@ else:
     print("Invalid number of arguments")
     interval = False
     
-start = time.time()
 
-# Diagnostics
+cleanup_tracked_list()
 
-tracked_error_list = []
-for device_string in v.devices.keys():
-    if (v.devices[device_string].get_pose_quaternion() is None):
-        print("[Warning]: Tracking device \"" + device_string + "\" can't be tracked!")
-        tracked_error_list.append(device_string)
 
-for device in tracked_error_list:
-    v.devices.pop(device)
+# floats = [0.1253, 0.157446, 0.968451, 0.87453618, 0.8968, 0.84865465, 0.843872]
 
-# Main Loop
+##### MAIN LOOP #####
 if interval:
     while(True):
         start = time.time()
@@ -49,10 +56,12 @@ if interval:
 
         for device_string in v.devices.keys():
             data += bytes(device_string + ":", "ascii")
-            ## print(device_string + "")
-            # print(v.devices[device_string].get_pose_quaternion())
-            for value in v.devices[device_string].get_pose_quaternion():
-                data += bytearray(struct.pack("f", value))
+
+            if (v.devices[device_string].get_pose_quaternion() is None):
+                remove_elements_flag = True
+            else:
+                for value in v.devices[device_string].get_pose_quaternion():
+                    data += bytearray(struct.pack("f", value))
 
         ### Fake data sender
         # for device_string in ["hmd_1", "controller_1"]:
@@ -64,9 +73,10 @@ if interval:
         # print(data, end="")
         # print("\r")
 
+        if remove_elements_flag:
+            cleanup_tracked_list()
+
         sock.sendto(data, server_address)
         # sleep_time = interval - (time.time() - start)
         # time.sleep(sleep_time if sleep_time > 0 else 0)
         time.sleep(interval)
-
-        
